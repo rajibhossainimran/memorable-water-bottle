8c38ad81439143cfbfb15a53eb318590d20ac193
rajibhossainimran <rajibhossainimran@gmail.com>
2024-10-22 21:35:04 +0600

    bottles component added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Bottles from './component/bottles/bottles'
+import Header from './component/Header'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+    <Bottles></Bottles>
+    
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/component/bottles/Bottles.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Bottles = () => {
+    const [bottles,setBottles] = useState([]);
+
+    useEffect(()=>{
+        fetch('bottle.json')
+        .then(res => res.json())
+        .then(data=>setBottles(data))
+    }, [])
+    return (
+        <div>
+            <h3>Bottles : {bottles.length}</h3>
+        </div>
+    );
+};
+
+export default Bottles;
